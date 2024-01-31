c8d7db3899383ead81a8fd7348b1c3931c945db8
lehiewl <2110801@sookmyung.ac.kr>
2024-01-31 15:57:46 +0900

    회원가입창 컴포넌트 분리-router에러 해결

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -2,8 +2,13 @@
 
 import React, { useState } from "react";
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import styled from "styled-components";
+import "../components/join.css";
 import JoinHello from "../components/JoinHello";
+import JoinSignUp from "../components/JoinSignUp";
+import back from "../assets/back_button.png";
+import logo from "../assets/paperielogo.png";
 
 const JoinContainer = styled.div`
   background-color: blue; /* Join 컨테이너의 배경색을 파란색으로 설정 */
@@ -12,10 +17,38 @@ const JoinContainer = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+const BackContainer = styled.div`
+display: flex;
+align-items: flex-start;
+margin-top: 10px;
+margin-left: 10px;
+position: absolute;
+top: 10px;
+left: 10px;
+`;
+
+
 const Join = () => {
   return (
     <JoinContainer>
-      <JoinHello />
+      <div className="BoxStyle">
+      <BackContainer>
+      <Link to="/">
+        <div className="BackButton">
+          <img src={back} alt="뒤로가기" />
+        </div>
+        </Link>
+      </BackContainer>
+      <div className="LeftContent">
+        <JoinHello/>
+        <JoinSignUp/>
+      </div>
+      <div className="VerticalLine" />
+      <div className="RightContent">
+        <img src={logo} style={{width: '250px', height: '250px'}}/>
+      </div>
+      </div>
     </JoinContainer>
   );
 };
