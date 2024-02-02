f936df76239663aacc3d2ab5b2405db31153d211
YOOJEONG LEE <uujeong59@sookmyung.ac.kr>
2024-02-02 22:02:31 +0900

    Result.js table 오류 고치기

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,25 +1,30 @@
-import React, { useEffect } from 'react';
+// Result.js
+
+import React, { useEffect, useState } from 'react';
 import { motion, useAnimation } from 'framer-motion';
 import "./result.css";
 
 const Results = ({ searchResults }) => {
-  const controls = useAnimation(); // 애니메이션 컨트롤을 위한 hook 사용
+  const controls = useAnimation();
+  const [showNoResultsMessage, setShowNoResultsMessage] = useState(true);
 
   useEffect(() => {
     const handleScroll = () => {
-      // 스크롤 위치에 따라 애니메이션 컨트롤
       const scrollY = window.scrollY;
-      controls.start({ opacity: scrollY > 200 ? 1 : 0.1 }); // 스크롤 위치에 따라 opacity 조절
+      controls.start({ opacity: scrollY > 200 ? 1 : 0.1 });
     };
 
-    // 스크롤 이벤트 리스너 추가
     window.addEventListener('scroll', handleScroll);
 
-    // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, [controls]);
+
+  useEffect(() => {
+    // Check if there are search results
+    setShowNoResultsMessage(!searchResults || searchResults.length === 0);
+  }, [searchResults]);
 
   return (
     <motion.div
@@ -34,14 +39,14 @@ const Results = ({ searchResults }) => {
           {searchResults && searchResults.length > 0 ? (
             searchResults.map((result, index) => (
               <tr key={index} className="result_name">
-                <td>{result.title}</td> {/* 실제 데이터 구조에 따라 속성 이름 조정 */}
+                <td>{result.title}</td>
               </tr>
             ))
-          ) : (
+          ) : !showNoResultsMessage ? (
             <tr>
               <td colSpan="1">검색 결과가 없습니다.</td>
             </tr>
-          )}
+          ) : null}
         </tbody>
       </table>
     </motion.div>
