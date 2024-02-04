cae5760b3d65fcd30a8c57665f71ad3dbe4d7420
isuHan <gkswltn3736@gmail.com>
2024-02-04 21:30:53 +0900

    논문 책 페이지 완성

--- src/components/ResultBook.js
+++ src/components/ResultBook.js
@@ -1,10 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion, useAnimation } from 'framer-motion';
 import "./result.css";
 import axios from "axios";
+import References from './References';
 
-function ResultBook({ results }) {
+
+function ResultBook({ searchResults, setApa, setMla, setChicago, setVan }) {
   const controls = useAnimation();
+
+  const [apaResults, setApaResults] = useState([]);
+  const [mlaResults, setMlaResults] = useState([]);
+  const [chiResults, setChiResults] = useState([]);
+  const [vnaResults, setVanResults] = useState([]);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -19,16 +26,44 @@ function ResultBook({ results }) {
     };
   }, [controls]);
   
-  const handleCellClick = (bookTitle) => {
-    console.log('선택된 제목: ', bookTitle);
-    axios.get(`http://127.0.0.1:8000/apa/books?selected_title=${bookTitle}`)
-    .then(response => {
-      console.log("성공: ", response.data);
-    })
-    .catch(error => {
-      console.error("실패: ", error);
-    })
 
+  const handleCellClick = async (data) => {
+    try {
+      ///APA
+      const getApaUrl = `http://127.0.0.1:8000/apa/books?selected_title=${data}`;
+  
+      const getApa = await axios.get(getApaUrl);
+      setApaResults(getApa.data);
+      console.log(getApa.data);
+      ///setApa(getApa.data);
+
+      ///MLA
+      const getMlaUrl = `http://127.0.0.1:8000/mla/books?selected_title=${data}`;
+  
+      const getMla = await axios.get(getMlaUrl);
+      setMlaResults(getMla.data);
+      console.log(getMla.data);
+      ///setMla(getMla.data.data.results);
+
+      ///CHI
+      const getChiUrl = `http://127.0.0.1:8000/chi/books?selected_title=${data}`;
+  
+      const getChi = await axios.get(getChiUrl);
+      setChiResults(getChi.data);
+      console.log(getChi.data);
+      ///setChicago(getChi.data.data.results);
+
+      ///VAN
+      const getVanUrl = `http://127.0.0.1:8000/van/books?selected_title=${data}`;
+  
+      const getVan = await axios.get(getVanUrl);
+      setVanResults(getVan.data);
+      console.log(getVan.data);
+      ///setVan(getVan.data.data.results);
+
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -38,27 +73,26 @@ function ResultBook({ results }) {
       transition={{ ease: "easeInOut", duration: 0.2 }}
       className="result_border"
     >
-      <table className="caption-top table-borderless table-hover">
-        <caption className="result_table_name"> 검색결과 </caption>
-        <thead>
-          <tr>
-            <th className="search_name" scope="col" width="800px">책 제목</th>
-          </tr>
-        </thead>
-        <tbody>
-          {results && results.length > 0 ? (
-            results.map((result, index) => (
-              <tr key={index} className="result_name">
-                <td onClick={() => handleCellClick(result.bookTitle)}>{result.bookTitle}</td>
-              </tr>
-            ))
-          ) : (
+      {searchResults && searchResults.length > 0 && (
+        <table className="caption-top table-borderless table-hover">
+          <caption className="result_table_name"> 검색결과 </caption>
+          <thead>
             <tr>
-              <td colSpan="1">No results found</td>
+              <th className="search_name" scope="col" width="800px">책 제목</th>
             </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {searchResults.map((data, index) => (
+              <tr key={index} className="result_name">
+                <td onClick={() => handleCellClick(data.title)}>
+                  {data.title}
+                </td>
+              </tr>
+            ))}
+            <References apa={apaResults} mla={mlaResults} chicago={chiResults} van={vnaResults}/>
+          </tbody>
+        </table>
+      )}
     </motion.div>
   );
 }
