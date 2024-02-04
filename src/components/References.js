fedcbce990761862906dd74551daa5555c1315db
lehiewl <2110801@sookmyung.ac.kr>
2024-02-04 19:39:33 +0900

    google로그인 시작

--- src/components/References.js
+++ src/components/References.js
@@ -10,10 +10,6 @@ import axios from "axios";
 function References({ apa, chicago, mla, van, result_title }) {
 
   const controls = useAnimation();
-  const [type, setType]= useState();
-  const [ref, setRef]=useState();
-  const [title, setTitle]= useState();
-
   
   useEffect(() => {
     const handleScroll = () => {
@@ -41,12 +37,31 @@ function References({ apa, chicago, mla, van, result_title }) {
     }
   };
 
+  const handleToMypage = async (reference, result) =>{
+    try{
+      const response = await axios.post("http://127.0.0.1:8000/mypage/get?title={title}&ref={ref}", {
+        title: result_title,
+        ref : reference,
+        type : "article",
+        content: {result},
+
+        });
+        console.log(response.data);
+      } catch (error) {
+        console.error("Mypage로 옮기기 실패", error);
+      }
+  }
+
   const renderTableRow = (type, result) => (
     <tr key={type}>
       <td className='refer_type' scope="row" width="200px">{type}</td>
       <td className={`${type.toLowerCase()}_result`} width="500px">{result}</td>
       <td>
-        <button type="button" className="copy_button" onClick={() => handleCopyClipBoard(result)}>
+        <button type="button" className="copy_button" 
+        onClick={() => {
+          handleCopyClipBoard(result);
+          handleToMypage(type, result);
+        }}>
           <div>
             <img className="copy_img" src={copy_img} alt={`${type} Copy`} />
             Copy
@@ -55,21 +70,6 @@ function References({ apa, chicago, mla, van, result_title }) {
       </td>
     </tr>
   );
-
-  const handleToMypage = async (reference) =>{
-    try{
-      const response = await axios.post("http://127.0.0.1:8000/mypage/get?title={title}&ref={ref}", {
-        title: result_title,
-        ref : reference,
-        type : "article",
-        content: {reference},
-
-        });
-        console.log(response.data);
-      } catch (error) {
-        console.error("Mypage로 옮기기 실패", error);
-      }
-  }
 
   return (
     <motion.div
